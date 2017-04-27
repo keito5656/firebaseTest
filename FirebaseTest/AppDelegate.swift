07002108014a0193b49ad42051f11606bb005711
YAMAMOTOHIROKI <yamamotohiroki@MacBook-Pro-6.local>
2017-04-27 14:58:23 +0900

    add code

--- FirebaseTest/AppDelegate.swift
+++ FirebaseTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAnalytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupFirebase()
         return true
     }
+    
+    private func setupFirebase() {
+        FIRApp.configure()
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
